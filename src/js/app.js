5f53a94f47b87f68329193bc3903667535c11d31
intastellar <70743293+intastellar@users.noreply.github.com>
2021-05-26 17:42:15 +0200

    Tilføjet funktion til at blive navigeret

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 const showMore = document.querySelectorAll(".pakkeInfo__ShowMore");
 const packageInfo = document.querySelectorAll(".pakkeInfo__more");
+const navigation = document.querySelectorAll(".--navigation");
 
 for(let i=0; i<showMore.length; i++){
     showMore[i].addEventListener("click", function(e){
@@ -13,3 +14,13 @@ for(let i=0; i<showMore.length; i++){
         }
     })
 }
+
+navigation.forEach((navi) => {
+    navi.addEventListener("click", (e)=>{
+        e.preventDefault();
+        const add = navi.getAttribute("data-address");
+        const navLink = `https://www.google.com/maps/dir/?api=1&origin=Current Location&destination=${add}`;
+
+        window.open(navLink, '_blank');
+    })
+})
