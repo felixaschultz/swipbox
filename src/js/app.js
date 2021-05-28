41c59a04c9bd3034b7e14e1a14624ff3595cb84a
intastellar <70743293+intastellar@users.noreply.github.com>
2021-05-28 12:12:08 +0200

    Login & confirm funktion tilføjet

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ const showMore = document.querySelectorAll(".pakkeInfo__ShowMore");
 const packageInfo = document.querySelectorAll(".pakkeInfo__more");
 const navigation = document.querySelectorAll(".--navigation");
 const loadingScreen = document.querySelector(".loadingScreen");
+const loginScreens = document.querySelectorAll(".loginScreen");
+const loginScreen = document.querySelector(".login");
+const btn = document.querySelectorAll(".--login");
+const confirmBtn = document.querySelector(".--confirm");
 
 for(let i=0; i<showMore.length; i++){
     showMore[i].addEventListener("click", function(e){
@@ -29,3 +33,14 @@ navigation.forEach((navi) => {
 setTimeout(function(){
     loadingScreen.style.opacity = "0";
 }, 3000);
+
+for(let o=0; o<btn.length; o++){
+    btn[o].addEventListener("click", function(){
+        loginScreens[o].style.left = "-100%";
+        loginScreens[o+1].style.right = "auto";
+    })
+}
+
+confirmBtn.addEventListener("click", function(){
+    loginScreen.style.top = "-100%";
+});
